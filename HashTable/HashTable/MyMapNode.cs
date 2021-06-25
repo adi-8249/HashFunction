a9c6eb8de102a04b55942dcdbcbb305b5428186e
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-25 16:55:35 +0530

    [Anjaneya]Added:printing Frequency of words in a sentence

---
+++ HashTable/HashTable/MyMapNode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    /// <summary>
+    /// In the hash table it is the combination of two key and value pair
+    /// here key value is predefined
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    class MyMapNode<K,V>
+    {
+        public readonly int size;
+        public readonly LinkedList<KeyValue<K, V>>[] items;
+        /// <summary>
+        /// creating constructor and passing the size of linkedlist
+        /// </summary>
+        /// <param name="size"></param>
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        /// <summary>
+        /// adding the key value pair in position
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+        public V Get(K key)//here V is value type
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+    }
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+}
+
--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -7,6 +7,21 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTab ");
+            MyMapNode<int, string> node = new MyMapNode<int, string>(6);
+            node.Add(0, "to");
+            node.Add(1, "be");
+            node.Add(2, "or");
+            node.Add(3, "not");
+            node.Add(4, "to");
+            node.Add(5, "be");
+            string node0 = node.Get(0);
+            Console.WriteLine("Frequency of to : " + node0);
+            string node1 = node.Get(1);
+            Console.WriteLine("Frequency of be : " + node1);
+            string node2 = node.Get(2);
+            Console.WriteLine("Frequency of or : " + node2);
+            string node3 = node.Get(3);
+            Console.WriteLine("Frequency of not : " + node3);
         }
     }
 }
